2de06b170478045ac9e81bf668440ef4d9a01b20
ronyparrakaribu <rony.parra@karibu.cl>
2024-02-17 11:46:22 -0300

    [featura][master] Crear modulo ciudad

--- src/modules/city/city.service.ts
+++ src/modules/city/city.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCityDto } from './dto/create-city.dto';
 import { UpdateCityDto } from './dto/update-city.dto';
+import { City } from './entities/city.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CityService {
+  constructor(
+    @InjectRepository(City)
+    private cityRepository: Repository<City>,
+  ) {}
   create(createCityDto: CreateCityDto) {
-    return 'This action adds a new city';
+    return this.cityRepository.save(createCityDto);
   }
 
   findAll() {
-    return `This action returns all city`;
+    return this.cityRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} city`;
+    return this.cityRepository.findOne({
+      where: { id },
+    });
   }
 
   update(id: number, updateCityDto: UpdateCityDto) {
-    return `This action updates a #${id} city`;
+    return this.cityRepository.update(id, updateCityDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} city`;
+    return this.cityRepository.delete(id);
   }
 }
--- src/modules/city/dto/update-city.dto.ts
+++ src/modules/city/dto/update-city.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCityDto } from './create-city.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateCityDto extends PartialType(CreateCityDto) {}
+export class UpdateCityDto extends PartialType(CreateCityDto) {
+  @ApiProperty({ example: 'Asuncion' })
+  name: string;
+}
